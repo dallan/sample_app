46356b506bcab0c32a94f454cd967b2caf5be63f
Dan Allan <usallans@netscape.net>
2012-04-17 22:41:22 -0600

    User signup complete"

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -25,10 +25,10 @@ describe "Users" do
       it "should not make a new user" do
         lambda do
           visit signup_path
-          fill in "Name",         :with => ""
-          fill in "Email",        :with => ""
-          fill in "Password",     :with => ""
-          fill in "Confirmation", :with => ""
+          fill_in "Name",         :with => ""
+          fill_in "Email",        :with => ""
+          fill_in "Password",     :with => ""
+          fill_in "Confirmation", :with => ""
           click_button
           response.should render_template('users/new')
           response.should have_selector("div#error_explanation")
